38fc60fdf948157245af871f50fbd150b2ff18b4
sotrnguy92 <sonnguyen092@gmail.com>
2021-11-14 11:32:31 -0800

    setting up container and paper for page

---
+++ src/components/MainComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container, Paper} from "@mui/material";
+
+
+export const MainComponent = () => {
+    return(
+        <Container sx={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <Paper sx={{width:"100%", height:"80vh",}}>
+            {/*    grid group from api */}
+            </Paper>
+        </Container>
+    )
+}
