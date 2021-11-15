ff311f4638e0098530d7060575c0fce0e1b9cd30
sotrnguy92 <sonnguyen092@gmail.com>
2021-11-15 10:48:12 -0800

    adding scroll functionality to student paper

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {Container, Paper} from "@mui/material";
 import {useStudentData} from '../hooks/useStudentsData'
 import {StudentGroupContainer} from './StudentsGroupComponent'
-
+import './MainComponent.css'
 
 export const MainComponent = () => {
     const studentData = useStudentData();
     return(
         <Container sx={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}} >
-            <Paper  elevation={2} sx={{p:1,pr:0, width:"100%", height:"80vh", flexGrow:1}}>
+            <Paper className="student-paper" elevation={2} sx={{p:1,pr:0, width:"100%", height:"80vh", flexGrow:1}}>
                 {studentData.length >0 && <StudentGroupContainer studentData={studentData}/>}
             </Paper>
         </Container>
